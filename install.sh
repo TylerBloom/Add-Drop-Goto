bfab9731e9ba3028edba721f86ddd134a85c9966
Tyler Bloom <tfbloom@mail.fhsu.edu>
2018-06-06 15:38:36 -0500

    Tab completion is now different based on the options that you are working with.

--- install.sh
+++ install.sh
@@ -1,26 +1,38 @@
 #! /bin/bash
- 
-if [ -e waypoint.txt ] # Creates waypoints if it does not exist
+
+if [ ! -e ~/.waypoints ]
 then
-	rm waypoints.txt
+	mkdir ~/.waypoints # Creates a directory to store waypoints.txt
 fi
 
-touch waypoints.txt
+if [ ! -e ~/.waypoints/waypoint.txt ] # Creates waypoints if it does not exist
+then
+	touch waypoints.txt
+	cp waypoints.txt adg ~/.waypoints
+fi
+
 base=$(pwd)
 cd ~
 home=$(pwd)
 cd $base
 echo "wayPointsLocation=""'"$home"/.waypoints'" >> waypoints.txt
-
-mkdir ~/.waypoints # Creates a directory to store waypoints.txt
-cp waypoints.txt adg ~/.waypoints
 echo "Adding completion file to /etc/bash_completion.d/"
 sudo cp ADG /etc/bash_completion.d/adg
-echo 'source ~/.waypoints/waypoints.txt' >> ~/.bashrc # Addes waypoints upon startup
-echo 'PATH=''"''${PATH}:~/.waypoints''"' >> ~/.bashrc # Addes the new directory to the PATH
 
-# Addes required aliases
-echo "alias adg='. adg'" >> ~/.bashrc
+if [[ $(cat ~/.bashrc) != *'source ~/.waypoints/waypoints.txt'* ]]
+then
+	echo 'source ~/.waypoints/waypoints.txt' >> ~/.bashrc # Addes waypoints upon startup
+fi
+
+if [[ $(cat ~/.bashrc) != *'PATH=''"''${PATH}:~/.waypoints''"'* ]]
+then
+	echo 'PATH=''"''${PATH}:~/.waypoints''"' >> ~/.bashrc # Addes the new directory to the PATH
+fi
+
+if [[ $(cat ~/.bashrc) != *"alias adg='. adg'"* ]]
+then
+	echo "alias adg='. adg'" >> ~/.bashrc # Addes required aliases
+fi
 
 echo "Configure complete. Please source ~/.bashrc and /etc/bash_completion for every terminal open or close it and open it again."
 echo "This only needs to happen once. Then you're good to go!"
--- test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-tions=':a:'
-
-while getopts "$tions" currOpt; do
-	echo ${currOpt[@]}
-	echo ${OPTARG}
-done
