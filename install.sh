f0189bc073d950334c16a459fdc4297e99798d3a
TylerBloom <tylerbloom2222@gmail.com>
2018-06-18 22:59:04 -0500

    Making adjusting to the main adg script

--- install.sh
+++ install.sh
@@ -1,38 +1,23 @@
 #! /bin/bash
 
-if [ ! -e ~/.waypoints ]
-then
-	mkdir ~/.waypoints # Creates a directory to store waypoints.txt
-fi
+[ ! -e ~/.waypoints ] && mkdir ~/.waypoints # Creates a directory to store waypoints.txt
 
 if [ ! -e ~/.waypoints/waypoint.txt ] # Creates waypoints if it does not exist
 then
-	touch waypoints.txt
-	cp waypoints.txt adg ~/.waypoints
+	home=$(cd ~; pwd)
+	echo "wayPointsLocation=""'"$home"/.waypoints'" > waypoints.txt
+	cp waypoints.txt ~/.waypoints
 fi
 
-base=$(pwd)
-cd ~
-home=$(pwd)
-cd $base
-echo "wayPointsLocation=""'"$home"/.waypoints'" >> waypoints.txt
+cp adg ~/.waypoints
 echo "Adding completion file to /etc/bash_completion.d/"
 sudo cp ADG /etc/bash_completion.d/adg
 
-if [[ $(cat ~/.bashrc) != *'source ~/.waypoints/waypoints.txt'* ]]
-then
-	echo 'source ~/.waypoints/waypoints.txt' >> ~/.bashrc # Addes waypoints upon startup
-fi
+[[ $(cat ~/.bashrc) != *'source ~/.waypoints/waypoints.txt'* ]] && echo 'source ~/.waypoints/waypoints.txt' >> ~/.bashrc # Addes waypoints upon startup
 
-if [[ $(cat ~/.bashrc) != *'PATH=''"''${PATH}:~/.waypoints''"'* ]]
-then
-	echo 'PATH=''"''${PATH}:~/.waypoints''"' >> ~/.bashrc # Addes the new directory to the PATH
-fi
+[[ $(cat ~/.bashrc) != *'PATH=''"''${PATH}:~/.waypoints''"'* ]] && echo 'PATH=''"''${PATH}:~/.waypoints''"' >> ~/.bashrc # Addes the new directory to the PATH
 
-if [[ $(cat ~/.bashrc) != *"alias adg='. adg'"* ]]
-then
-	echo "alias adg='. adg'" >> ~/.bashrc # Addes required aliases
-fi
+[[ $(cat ~/.bashrc) != *"alias adg='. adg'"* ]] && echo "alias adg='. adg'" >> ~/.bashrc # Addes required aliases
 
 echo "Configure complete. Please source ~/.bashrc and /etc/bash_completion for every terminal open or close it and open it again."
 echo "This only needs to happen once. Then you're good to go!"
